6ea39db06e0f7116f5b0298dfe020f239dbc2ec9
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-04-25 14:13:55 -0400

    Add project files.

---
+++ Lab10-KayakRentalPrices/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'AllPricesDataSet.RentalPrices' table. You can move, or remove it, as needed.
+        Me.RentalPricesTableAdapter.Fill(Me.AllPricesDataSet.RentalPrices)
+
+    End Sub
+End Class
