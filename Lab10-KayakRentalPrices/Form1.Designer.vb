b0173d28e53f77b7dc5a251716c609f5715d2099
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-04-25 14:29:21 -0400

    Finished Assignment

--- Lab10-KayakRentalPrices/Form1.Designer.vb
+++ Lab10-KayakRentalPrices/Form1.Designer.vb
@@ -23,33 +23,17 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.AllPricesDataSet = New Lab10_KayakRentalPrices.AllPricesDataSet()
         Me.RentalPricesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.RentalPricesTableAdapter = New Lab10_KayakRentalPrices.AllPricesDataSetTableAdapters.RentalPricesTableAdapter()
         Me.TableAdapterManager = New Lab10_KayakRentalPrices.AllPricesDataSetTableAdapters.TableAdapterManager()
-        Me.RentalPricesBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
-        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
-        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
-        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
-        Me.RentalPricesBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
         Me.RentalPricesDataGridView = New System.Windows.Forms.DataGridView()
-        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.AllPricesDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RentalPricesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.RentalPricesBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.RentalPricesBindingNavigator.SuspendLayout()
         CType(Me.RentalPricesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -73,170 +57,61 @@ Partial Class Form1
         Me.TableAdapterManager.Connection = Nothing
         Me.TableAdapterManager.UpdateOrder = Lab10_KayakRentalPrices.AllPricesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
         '
-        'RentalPricesBindingNavigator
-        '
-        Me.RentalPricesBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
-        Me.RentalPricesBindingNavigator.BindingSource = Me.RentalPricesBindingSource
-        Me.RentalPricesBindingNavigator.CountItem = Me.BindingNavigatorCountItem
-        Me.RentalPricesBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
-        Me.RentalPricesBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.RentalPricesBindingNavigatorSaveItem})
-        Me.RentalPricesBindingNavigator.Location = New System.Drawing.Point(0, 0)
-        Me.RentalPricesBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
-        Me.RentalPricesBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
-        Me.RentalPricesBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
-        Me.RentalPricesBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
-        Me.RentalPricesBindingNavigator.Name = "RentalPricesBindingNavigator"
-        Me.RentalPricesBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
-        Me.RentalPricesBindingNavigator.Size = New System.Drawing.Size(526, 25)
-        Me.RentalPricesBindingNavigator.TabIndex = 0
-        Me.RentalPricesBindingNavigator.Text = "BindingNavigator1"
-        '
-        'BindingNavigatorMoveFirstItem
-        '
-        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
-        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
-        '
-        'BindingNavigatorMovePreviousItem
-        '
-        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
-        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
-        '
-        'BindingNavigatorSeparator
-        '
-        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
-        '
-        'BindingNavigatorPositionItem
-        '
-        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
-        Me.BindingNavigatorPositionItem.AutoSize = False
-        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
-        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
-        Me.BindingNavigatorPositionItem.Text = "0"
-        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
-        '
-        'BindingNavigatorCountItem
-        '
-        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
-        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 15)
-        Me.BindingNavigatorCountItem.Text = "of {0}"
-        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
-        '
-        'BindingNavigatorSeparator1
-        '
-        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
-        '
-        'BindingNavigatorMoveNextItem
-        '
-        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
-        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorMoveNextItem.Text = "Move next"
-        '
-        'BindingNavigatorMoveLastItem
-        '
-        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
-        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorMoveLastItem.Text = "Move last"
-        '
-        'BindingNavigatorSeparator2
-        '
-        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
-        '
-        'BindingNavigatorAddNewItem
-        '
-        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
-        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorAddNewItem.Text = "Add new"
-        '
-        'BindingNavigatorDeleteItem
-        '
-        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
-        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorDeleteItem.Text = "Delete"
-        '
-        'RentalPricesBindingNavigatorSaveItem
-        '
-        Me.RentalPricesBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.RentalPricesBindingNavigatorSaveItem.Enabled = False
-        Me.RentalPricesBindingNavigatorSaveItem.Image = CType(resources.GetObject("RentalPricesBindingNavigatorSaveItem.Image"), System.Drawing.Image)
-        Me.RentalPricesBindingNavigatorSaveItem.Name = "RentalPricesBindingNavigatorSaveItem"
-        Me.RentalPricesBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 20)
-        Me.RentalPricesBindingNavigatorSaveItem.Text = "Save Data"
-        '
         'RentalPricesDataGridView
         '
+        Me.RentalPricesDataGridView.AllowUserToAddRows = False
+        Me.RentalPricesDataGridView.AllowUserToDeleteRows = False
         Me.RentalPricesDataGridView.AutoGenerateColumns = False
         Me.RentalPricesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.RentalPricesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
+        Me.RentalPricesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn3})
         Me.RentalPricesDataGridView.DataSource = Me.RentalPricesBindingSource
-        Me.RentalPricesDataGridView.Location = New System.Drawing.Point(70, 90)
+        Me.RentalPricesDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RentalPricesDataGridView.Location = New System.Drawing.Point(0, 0)
         Me.RentalPricesDataGridView.Name = "RentalPricesDataGridView"
-        Me.RentalPricesDataGridView.Size = New System.Drawing.Size(444, 220)
+        Me.RentalPricesDataGridView.ReadOnly = True
+        Me.RentalPricesDataGridView.Size = New System.Drawing.Size(461, 356)
         Me.RentalPricesDataGridView.TabIndex = 1
-        '
-        'DataGridViewTextBoxColumn1
-        '
-        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Daily_Rate"
-        Me.DataGridViewTextBoxColumn1.HeaderText = "Daily_Rate"
-        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
-        '
-        'DataGridViewTextBoxColumn2
-        '
-        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Hourly_Rate"
-        Me.DataGridViewTextBoxColumn2.HeaderText = "Hourly_Rate"
-        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
-        '
-        'DataGridViewTextBoxColumn3
-        '
-        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Weekly_Rate"
-        Me.DataGridViewTextBoxColumn3.HeaderText = "Weekly_Rate"
-        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
         '
         'DataGridViewTextBoxColumn4
         '
         Me.DataGridViewTextBoxColumn4.DataPropertyName = "Name"
         Me.DataGridViewTextBoxColumn4.HeaderText = "Name"
         Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Hourly_Rate"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Hourly Rate"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Daily_Rate"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Daily Rate"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Weekly_Rate"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Weekly Rate"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.ReadOnly = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(526, 356)
+        Me.ClientSize = New System.Drawing.Size(461, 356)
         Me.Controls.Add(Me.RentalPricesDataGridView)
-        Me.Controls.Add(Me.RentalPricesBindingNavigator)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.AllPricesDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.RentalPricesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.RentalPricesBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.RentalPricesBindingNavigator.ResumeLayout(False)
-        Me.RentalPricesBindingNavigator.PerformLayout()
         CType(Me.RentalPricesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -244,22 +119,9 @@ Partial Class Form1
     Friend WithEvents RentalPricesBindingSource As BindingSource
     Friend WithEvents RentalPricesTableAdapter As AllPricesDataSetTableAdapters.RentalPricesTableAdapter
     Friend WithEvents TableAdapterManager As AllPricesDataSetTableAdapters.TableAdapterManager
-    Friend WithEvents RentalPricesBindingNavigator As BindingNavigator
-    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
-    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
-    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
-    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
-    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
-    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
-    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
-    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
-    Friend WithEvents RentalPricesBindingNavigatorSaveItem As ToolStripButton
     Friend WithEvents RentalPricesDataGridView As DataGridView
-    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
     Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
 End Class
--- Lab10-KayakRentalPrices/Form1.vb
+++ Lab10-KayakRentalPrices/Form1.vb
@@ -2,6 +2,9 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'AllPricesDataSet.RentalPrices' table. You can move, or remove it, as needed.
         Me.RentalPricesTableAdapter.Fill(Me.AllPricesDataSet.RentalPrices)
+    End Sub
+
+    Private Sub RentalPricesDataGridView_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles RentalPricesDataGridView.CellContentClick
 
     End Sub
 End Class
